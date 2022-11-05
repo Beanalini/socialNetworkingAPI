43714230651ebf44366c67708db484e7842a619f
Beanalini <dat826@gmail.com>
2022-11-05 12:05:43 +0000

    Added user model

--- models/User.js
+++ models/User.js
@@ -2,34 +2,57 @@ const { Schema, model } = require('mongoose');
 
 //Schema to create user model
 const userSchema = new Schema(
-{
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trimmed: true,
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+            required: [true, "Email required"],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Friend',
+            },
+        ],
     },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        //add validation
-    },
-    thoughts: {
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+);
 
-    }
-
-
-
-},
-
-
-
-)
-
-
+//Create a virtual property called friendCount that counts the number of friends a user has by 
+// getting the length of the user's friends array field when queried
+userSchema
+    .virtual('getFriends')
+    // Getter
+    .get(function () {
+    return this.friends.length;
+});
 
 // Initialize User model
 const User = model('user', userSchema);
+
 
 module.exports = User;
